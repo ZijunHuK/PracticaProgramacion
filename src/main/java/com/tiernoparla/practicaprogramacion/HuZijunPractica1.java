cd5f2171e1692979250620aa21c05d34eab20e36
Zijun Hu <zijun.hu@tiernoparla.es>
2022-12-02 17:05:15 +0100

    Primera_parte_hecho

---
+++ src/main/java/com/tiernoparla/practicaprogramacion/HuZijunPractica1.java
@@ -0,0 +1,19 @@
+
+package com.tiernoparla.practicaprogramacion;
+
+    import java.util.*;
+
+public class HuZijunPractica1 {
+    public static void main(String[] args) {
+   
+    //introducir mensaje
+    Scanner sca = new Scanner(System.in);
+ 
+    System.out.println("Introduce longitud de tu mensaje: ");
+   
+    int longitud = sca.nextInt();
+ 
+    System.out.println("longitud que tengo son: " + longitud);
+ 
+    } //main
+} //HuZijunPractica1

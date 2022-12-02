54ffe574380457401867b21fd0644b3d28f371bd
Zijun Hu <zijun.hu@tiernoparla.es>
2022-12-02 17:10:36 +0100

    Segunda_parte_hecha

--- src/main/java/com/tiernoparla/practicaprogramacion/HuZijunPractica1.java
+++ src/main/java/com/tiernoparla/practicaprogramacion/HuZijunPractica1.java
@@ -14,6 +14,28 @@ public class HuZijunPractica1 {
     int longitud = sca.nextInt();
  
     System.out.println("longitud que tengo son: " + longitud);
+    
+    //Calcular cantidad mínima de bits de redundancia necesarios
+    Random ram = new Random();
+   
+    for (int i = 0; i < longitud; i++) {
+      int elevado = (int) Math.pow(2, i)-(i + 1);
+      int total = (longitud + i) + 1;
  
+      if (elevado >= longitud) {
+          System.out.println("bits de redundancia de " + longitud + " es " + i + " y más bit global 1");
+          System.out.println("bits en total son :" + total);
+ 
+    //Genarar mensaje a binario aleatoriamente
+          for (int k = 0; k < total; k++) {
+          int binario = ram.nextInt(2);
+          System.out.print(binario + " ");
+            } //for k
+ 
+            break;
+        } //if
+ 
+      } //for i
+    
     } //main
 } //HuZijunPractica1
